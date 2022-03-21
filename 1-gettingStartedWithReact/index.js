816d79294be898a1e792089172f1223328774387
jash varu <jashvaru@gmail.com>
2022-03-21 19:40:23 +0530

    Initial commit

---
+++ 1-gettingStartedWithReact/index.js
@@ -0,0 +1,12 @@
+const navBar = (
+    <nav>
+        <ul>
+            <h1>Jash's Website</h1>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact us</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navBar, document.getElementById("root"))
---
+++ 2-react-info-site/index.js
@@ -0,0 +1,15 @@
+const navBar = (
+    <div>
+        <img src="React-icon.png" width="50px" alt="React-logo"></img>
+        <p>React JS</p>
+        <h1>Fun facts About React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(navBar, document.getElementById("root"))

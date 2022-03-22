e0900ae19bc023ec79b9c92780198d40fbbdead5
jash varu <jashvaru@gmail.com>
2022-03-22 23:56:33 +0530

    Final commit

---
+++ 2-my-react-info/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/Navbar"
+import Content from "./components/MainContent"
+
+function Application() {
+    return (
+        <div>
+            <Navbar />
+            <Content />
+        </div>
+    )
+};
+
+export default Application;
+
--- 2-my-react-info/src/components/MainContent.js
+++ 2-my-react-info/src/components/MainContent.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 function Content() {
     return (
-        <div>
-            <h1>Fun facts About React</h1>
-            <ol>
+        <main className="main">
+            <h1 className="main-title">Fun facts About React</h1>
+            <ul className="main-facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
-        </div>
+            </ul>
+        </main>
     )
 }
 

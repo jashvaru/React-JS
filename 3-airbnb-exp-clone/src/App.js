8f4ca36a3e8bc89d753ddad8481a80c970246113
jash varu <jashvaru@gmail.com>
2022-03-23 23:37:36 +0530

    Made certain chnages

--- 3-airbnb-exp-clone/src/App.js
+++ 3-airbnb-exp-clone/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../src/components/navbar";
 import HeroSection from "../src/components/hero";
+import Review from "../src/components/review";
+
+{/* <HeroSection /> */}
 
 function Application() {
     return (
         <div>
             <Navbar />
-            <HeroSection />
+            <Review />
         </div>
     );
 }
--- 3-airbnb-exp-clone/src/components/navbar.js
+++ 3-airbnb-exp-clone/src/components/navbar.js
@@ -4,9 +4,9 @@ import AirbnbLogo from "../images/airbnb-logo.png";
 function Navbar() {
     return (
         <nav>
-            <img src={AirbnbLogo} alt="airbnb-logo"/>
+            <img src={AirbnbLogo} alt="airbnb-logo" />
         </nav>
-    )
+    );
 }
 
 export default Navbar;

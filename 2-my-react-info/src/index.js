f1d98acdf49fa490b123fec1d732b4dabe0115a2
JashVaru <49676708+jashvaru@users.noreply.github.com>
2022-03-23 19:38:49 +0530

    Delete 2-my-react-info directory

--- 2-my-react-info/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/App'
-import "./style.css"
-/* 
-component in react should be name in pascal format TemporaryName
-and while calling use <TemporaryName/>
-*/
-ReactDOM.render(<App />, document.getElementById("root"))
